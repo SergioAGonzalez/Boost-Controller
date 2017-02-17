bf961f63fc056c69cf10469f4623cd98b32026f2
Sergio_Gonzalez <sergio-gonzalez@aggiemail.usu.edu>
2017-02-17 10:52:24 -0700

    main Headers

---
+++ main.h
@@ -0,0 +1,12 @@
+// ------------------Primary Function Declarations ---------
+void setup_sysclk(void);
+
+// ------------------LDC Function Declarations -------------
+void setup_SSIO(void);
+void setup_PB(void);     
+void setup_PD(void);
+void lines(unsigned short);
+void background(void);
+
+// ------------------Touch Function Declarations ----------
+void Touch_init(void);
